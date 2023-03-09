426d585789615c852cc51e55185f911bb385d351
DieterVanc <dieter.vancoillie@student.vives.be>
2023-03-09 13:51:03 +0100

    2esemlabo4

---
+++ labo_4_2e_semester/opdracht_man_van_an/scripts/index.js
@@ -0,0 +1,32 @@
+const setup = () => {
+    countAn();
+}
+const countAn = () => {
+    let input = document.getElementById("input").textContent;
+    let inputLower = input.toLowerCase();
+
+    let count1 = 0;
+    if (inputLower.indexOf("an") != -1) {
+        for (let i = 0; i < input.length; i++) {
+            if (inputLower.charAt(i) === 'a' && inputLower.charAt(i + 1) === 'n') {
+                count1++;
+            }
+        }
+    }
+    console.log(count1);
+
+
+    let count2 = 0;
+    if (inputLower.lastIndexOf("an") != -1) {
+        for (let i = 0; i < input.length; i++) {
+            if (inputLower.charAt(i) === 'a' && inputLower.charAt(i + 1) === 'n') {
+                count2++;
+            }
+        }
+    }
+    console.log(count2);
+
+}
+
+
+window.addEventListener("load", setup);
